41c308fb426c5683f0b5abcb568cce4746f8b1cf
Jackson Romie <jromie0924@gmail.com>
2015-10-27 14:07:06 -0400

    gets battery status every 5 seconds using a kthread

--- lab4/battcheck.c
+++ lab4/battcheck.c
@@ -1,75 +1,64 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/proc_fs.h>
-#include <linux/workqueue.h>
+#include <linux/kthread.h>
 #include <linux/slab.h>
 #include <asm/uaccess.h>
 #include <linux/acpi.h>
+#include <linux/delay.h>
 
 MODULE_LICENSE("GPL");
 // use BIF and BST
 
-struct workqueue_struct *queue;
-struct delayed_work dwork;
+#define delay 5000
 
-void doit(void) {
-	acpi_status status;
-	acpi_handle handle;
-	union acpi_object* result;
-	struct acpi_buffer buffer = { ACPI_ALLOCATE_BUFFER, NULL };
+struct task_struct *thread;
+int tid = 0;
 
+int doit(void* id) {
 	int charge_discharge, charge, y;
+	while(1) {
+		acpi_status status;
+		acpi_handle handle;
+		union acpi_object* result;
+		struct acpi_buffer buffer = { ACPI_ALLOCATE_BUFFER, NULL };
 
-	 status = acpi_get_handle(NULL, (acpi_string)"\\_SB_.PCI0.BAT0", &handle);
-
-	 status = acpi_evaluate_object(handle, "_BST", NULL, &buffer);
-	 result = buffer.pointer;
-	 if(result) {
-	 	charge_discharge = result->package.elements[0].integer.value;
-	 	charge = result->package.elements[2].integer.value;
-	 //	kernel_fpu_begin();
-	 	y = (int)(charge/1.0);
-	 //	kernel_fpu_end();
-	 	printk(KERN_INFO "discharging=%d charge=%d remaining=%d\n", charge_discharge, charge, y);
-	 	kfree(result);
-	 }
-	 queue_delayed_work(queue, &dwork, HZ);
+		status = acpi_get_handle(NULL, (acpi_string)"\\_SB_.PCI0.BAT0", &handle);
+		status = acpi_evaluate_object(handle, "_BST", NULL, &buffer);
+		result = buffer.pointer;
+		if(result) {
+			charge_discharge = result->package.elements[0].integer.value;
+			charge = result->package.elements[2].integer.value;
+		//	kernel_fpu_begin();
+			y = (int)(charge/1.0);
+		//	kernel_fpu_end();
+			printk(KERN_INFO "Info from thread ID %d: discharging=%d charge=%d remaining=%d\n", *((int*)id), charge_discharge, charge, y);
+			kfree(result);
+		}
+		msleep(delay);
+	}
+	// queue_delayed_work(queue, &dwork, HZ);
+	return 0;
 }
 
 int init_module(void) {
-	/*acpi_status info;
-	acpi_handle ihandle;
-	union acpi_object* result;
-	struct acpi_buffer buffer = { ACPI_ALLOCATE_BUFFER, NULL };
+//	queue = create_workqueue("queue");
+//	INIT_DELAYED_WORK((struct delayed_work*)&dwork, doit);
+//	queue_delayed_work(queue, &dwork, HZ);
 
-	int capacity, capLow;
-	//string model;
-	info = acpi_get_handle(NULL, (acpi_string)"\\_SB_.PCI0.BAT0", &ihandle);
-	info = acpi_evaluate_object(ihandle, "BIF", NULL, &buffer);
-	result = buffer.pointer;
-	if(result) {
-		capacity = result->package.elements[1].integer.value;
-		capLow = result->package.elements[6].integer.value;
-	//	model = result->package.elements[9].toString();
+	thread = kthread_create(doit, (void*)&tid, "tid");
+//	t1 = kthread_create(incrementer, (void*)&id1, "inc1");
 
-		printk(KERN_INFO "Battery Capacity=%d Capacityof Low=%d\n", capacity, capLow);
-		kfree(result);
+	if(thread) {
+		wake_up_process(thread);
+	} else {
+		printk(KERN_EMERG "ERROR\n");
 	}
-	else {
-		printk(KERN_EMERG "ERROR GETTING BATTERY INFO\n\n");
-	}*/
 
-	queue = create_workqueue("queue");
-	INIT_DELAYED_WORK((struct delayed_work*)&dwork, doit;
-	queue_delayed_work(queue, &dwork, HZ);
+	//doit();
 	return 0;
 }
 
 void cleanup_module() {
-	if(delayed_work_pending(&dwork)) {
-		cancel_delayed_work(&dwork);
-	}
-	flush_workqueue(queue);
-	destroy_workqueue(queue);
 	printk(KERN_INFO "MODULE REMOVED\n");
 }
